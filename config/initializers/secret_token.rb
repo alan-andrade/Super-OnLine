3fc3b81cf32b7d6b45564d51032c8e90572f2b1e
Alan Andrade <gallito7@gmail.com>
2010-12-25 21:01:30 -0600

    Start of Wallmarts death

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperOnLine::Application.config.secret_token = 'a5881eb5f37e7a18aa43a4bfea47b9eaf6b0fe8148af3140acf51c0bb516d95903f3cf63541c3e97c66e99b6d2c1165248f10bdfb2954d224b8fb45986cfc72c'
